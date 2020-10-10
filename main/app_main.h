e2f9b0e888423f77be3bf8485dd7014ff23612c3
Cipi Bad <cipibad@gmail.com>
2020-10-10 23:01:26 +0300

    fix thermostat define check

--- main/app_main.h
+++ main/app_main.h
@@ -2,14 +2,13 @@
 #define APP_MAIN_H
 
 #if CONFIG_ESPTOOLPY_FLASHSIZE_1MB
-#ifdef CONFIG_MQTT_THERMOSTAT
+#if CONFIG_MQTT_THERMOSTATS_NB > 0 //this is my brother's configuration, where esp8266 controls external module of 4 relays
 #define RELAY_ON 0
 #define RELAY_OFF 1
 #else //CONFIG_MQTT_THERMOSTAT
 #define RELAY_ON 1
 #define RELAY_OFF 0
 #endif //CONFIG_MQTT_THERMOSTAT
-
 
 #else //CONFIG_ESPTOOLPY_FLASHSIZE_1MB
 #define RELAY_ON 0
